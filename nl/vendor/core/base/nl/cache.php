53510c3abb46e20197b9ecc33a357b138313de53
ICTD072 <82272495+ICTD072@users.noreply.github.com>
2023-04-09 17:40:13 +0200

    Update cache.php

--- nl/vendor/core/base/nl/cache.php
+++ nl/vendor/core/base/nl/cache.php
@@ -1,33 +1,33 @@
 <?php
 
 return [
-    'cache_management' => 'Cache management',
-    'cache_commands' => 'Clear cache commands',
+    'cache_management' => 'Cachebeheer',
+    'cache_commands' => 'Wis cache-opdrachten',
     'commands' => [
         'clear_cms_cache' => [
-            'title' => 'Clear all CMS cache',
-            'description' => 'Clear CMS caching: database caching, static blocks... Run this command when you don\'t see the changes after updating data.',
-            'success_msg' => 'Cache cleaned',
+            'title' => 'Wis alle CMS-cache',
+            'description' => 'CMS-caching wissen: databasecaching, statische blokken... Voer deze opdracht uit als u de wijzigingen niet ziet na het bijwerken van gegevens.',
+            'success_msg' => 'Cache opgeschoond',
         ],
         'refresh_compiled_views' => [
-            'title' => 'Refresh compiled views',
-            'description' => 'Clear compiled views to make views up to date.',
-            'success_msg' => 'Cache view refreshed',
+            'title' => 'Gecompileerde weergaven vernieuwen',
+            'description' => 'Wis gecompileerde weergaven om weergaven up-to-date te houden.',
+            'success_msg' => 'Cacheweergave vernieuwd',
         ],
         'clear_config_cache' => [
-            'title' => 'Clear config cache',
-            'description' => 'You might need to refresh the config caching when you change something on production environment.',
-            'success_msg' => 'Config cache cleaned',
+            'title' => 'Wis de configuratiecache',
+            'description' => 'Mogelijk moet u de configuratiecache vernieuwen wanneer u iets wijzigt in de productieomgeving.',
+            'success_msg' => 'Configuratiecache opgeschoond',
         ],
         'clear_route_cache' => [
-            'title' => 'Clear route cache',
-            'description' => 'Clear cache routing.',
-            'success_msg' => 'The route cache has been cleaned',
+            'title' => 'Routecache wissen',
+            'description' => 'Cacherouting wissen.',
+            'success_msg' => 'De routecache is opgeschoond',
         ],
         'clear_log' => [
-            'title' => 'Clear log',
-            'description' => 'Clear system log files',
-            'success_msg' => 'The system log has been cleaned',
+            'title' => 'Logboek opschonen',
+            'description' => 'Wis systeemlogbestanden',
+            'success_msg' => 'Het systeemlogboek is opgeschoond',
         ],
     ],
 ];
