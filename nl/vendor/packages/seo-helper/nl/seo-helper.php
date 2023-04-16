6f87944de39bb0fbb39da7ea83be42482f47e24f
ICTD072 <82272495+ICTD072@users.noreply.github.com>
2023-04-16 17:16:17 +0200

    Update seo-helper.php

--- nl/vendor/packages/seo-helper/nl/seo-helper.php
+++ nl/vendor/packages/seo-helper/nl/seo-helper.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'meta_box_header'     => 'Search Engine Optimize',
-    'edit_seo_meta'       => 'Edit SEO meta',
-    'default_description' => 'Setup meta title & description to make your site easy to discovered on search engines such as Google',
-    'seo_title'           => 'SEO Title',
-    'seo_description'     => 'SEO description',
+    'meta_box_header' => 'Zoekmachine optimalisatie',
+    'edit_seo_meta' => 'Bewerk SEO meta',
+    'default_description' => 'Stel een metatitel en -beschrijving in om uw site gemakkelijk vindbaar te maken in zoekmachines zoals Google',
+    'seo_title' => 'SEO Titel',
+    'seo_description' => 'SEO omschrijving',
 ];
